1061fc9a4347593db99d9ed10fe615ac7a6f1832
Alek Howland <83378924+HowlandAlek@users.noreply.github.com>
2021-11-16 07:14:28 -0500

    Initial Commit

---
+++ Mr-Cash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mr-Cash
+//
+//  Created by Alek Howland on 16/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
