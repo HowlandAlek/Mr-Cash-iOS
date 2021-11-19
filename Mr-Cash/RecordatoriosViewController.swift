be324bfd9ca1213194ba02ec0e8292f6056ca9f2
Alek Howland <83378924+HowlandAlek@users.noreply.github.com>
2021-11-19 14:27:06 -0500

    Se agrega formato a recordatorios y unifica funcionalidad demo

--- Mr-Cash/RecordatoriosViewController.swift
+++ Mr-Cash/RecordatoriosViewController.swift
@@ -14,18 +14,39 @@ class RecordatoriosViewController: UIViewController
     
     
     var arrRecordatorios  = [] as [Recordatorio]
-    
-    var recordatorio = Recordatorio(nombre: "Xbox", monto: 13999, fecha: Date())
-    var recordatorio2 = Recordatorio(nombre: "PS5", monto: 13999, fecha: Date())
-    
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
+        // Specify date components
+        var dateComponents = DateComponents()
+        dateComponents.year = 2023
+        dateComponents.month = 7
+        dateComponents.day = 13
+        dateComponents.timeZone = TimeZone(abbreviation: "JST") // Japan Standard Time
+        dateComponents.hour = 8
+        dateComponents.minute = 34
+
+        // Create date from components
+        let userCalendar = Calendar(identifier: .gregorian) // since the components above (like year 1980) are for Gregorian
+        let fechaDemo = userCalendar.date(from: dateComponents)
+        
+        var recordatorio = Recordatorio(nombre: "Netlix", monto: 280, fecha: fechaDemo!)
+        var recordatorio2 = Recordatorio(nombre: "Colegiatura Tec", monto: 30000, fecha: fechaDemo!)
+        var recordatorio3 = Recordatorio(nombre: "Disney+", monto: 249, fecha: fechaDemo!)
+        var recordatorio4 = Recordatorio(nombre: "CFE (Luz)+", monto: 450, fecha: fechaDemo!)
+        var recordatorio5 = Recordatorio(nombre: "Xbox Gamepass Ultimate", monto: 269, fecha: fechaDemo!)
+        
+        
         arrRecordatorios.append(recordatorio)
         arrRecordatorios.append(recordatorio2)
+        arrRecordatorios.append(recordatorio3)
+        arrRecordatorios.append(recordatorio4)
+        arrRecordatorios.append(recordatorio5)
+        
         
         tvRecordatorios.delegate = self
         tvRecordatorios.dataSource = self
@@ -41,7 +62,7 @@ extension RecordatoriosViewController :  UITableViewDelegate
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 70
+        return 100
     }
 }
 
@@ -57,8 +78,19 @@ extension RecordatoriosViewController : UITableViewDataSource
         var recordatorio = arrRecordatorios[indexPath.row]
         
         cell.etTitulo.text = recordatorio.Nombre
-        cell.etFechaPago.text = DateFormatter().string(from: recordatorio.Fecha)
-        cell.etMontoPago.text = String(recordatorio.Monto)
+        
+        let calendar = Calendar.current
+        
+        let day = calendar.component(.day, from: recordatorio.Fecha)
+        let month = calendar.component(.month, from: recordatorio.Fecha)
+        let year = calendar.component(.year, from: recordatorio.Fecha)
+        let fechaStr = "\(day)-\(month)-\(year)"
+        
+        cell.etFechaPago.text = fechaStr
+        
+        let montoFormat = String(format: "%0.3f", recordatorio.Monto)
+        
+        cell.etMontoPago.text = "$ \(montoFormat)"
 
         return cell
     }
